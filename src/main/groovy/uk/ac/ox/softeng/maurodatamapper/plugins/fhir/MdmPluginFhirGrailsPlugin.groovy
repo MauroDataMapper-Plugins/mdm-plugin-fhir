0eb6b596c48125c4196fd8cb89ad3f932e4129de
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2022-01-26 16:19:50 +0000

    Update build and dependencies to grails 5 to match mdm-core 5

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/fhir/MdmPluginFhirGrailsPlugin.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/fhir/MdmPluginFhirGrailsPlugin.groovy
@@ -22,7 +22,7 @@ import grails.plugins.Plugin
 class MdmPluginFhirGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.0 > *"
+    def grailsVersion = '5.1.2 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
@@ -59,8 +59,8 @@ The FHIR Server importer services  for the Mauro Data Mapper backend.
     def scm = [url: "https://github.com/mauroDataMapper-plugins/mdm-plugin-fhir"]
 
     def dependsOn = [
-        mdmCore           : '4.8.0 > *',
-        mdmPluginDatamodel: '4.8.0 > *',
+        mdmCore : '5.0.0 > *',
+        mdmPluginDatamodel : '5.0.0 > *',
     ]
 
     Closure doWithSpring() {
