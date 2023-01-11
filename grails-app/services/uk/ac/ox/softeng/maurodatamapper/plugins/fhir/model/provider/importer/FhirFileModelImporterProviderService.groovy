03e3589fc88154440c2c72c83126722e1181f5f0
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2023-01-11 16:36:25 +0000

    Can't use saveModelWithContent when creating VersionedFolder for Bundle as this causes Hibernate session issues

--- grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/fhir/model/provider/importer/FhirFileModelImporterProviderService.groovy
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/fhir/model/provider/importer/FhirFileModelImporterProviderService.groovy
@@ -22,20 +22,14 @@ import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiNotYetImplementedExcept
 import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiUnauthorizedException
 import uk.ac.ox.softeng.maurodatamapper.core.authority.AuthorityService
 import uk.ac.ox.softeng.maurodatamapper.core.container.ClassifierService
-import uk.ac.ox.softeng.maurodatamapper.core.container.FolderService
 import uk.ac.ox.softeng.maurodatamapper.core.container.VersionedFolder
 import uk.ac.ox.softeng.maurodatamapper.core.container.VersionedFolderService
-import uk.ac.ox.softeng.maurodatamapper.core.model.Model
-import uk.ac.ox.softeng.maurodatamapper.core.model.ModelService
 import uk.ac.ox.softeng.maurodatamapper.core.provider.ProviderType
 import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.ImporterProviderService
-import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.ModelImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.core.traits.provider.importer.JsonImportMapping
-import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.ImportDataHandling
 import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.MetadataHandling
 import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.codeset.provider.importer.FhirCodeSetImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.model.provider.importer.parameter.FhirFileModelImporterProviderServiceParameters
-import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.terminology.provider.exporter.FhirTerminologyExporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.plugins.fhir.terminology.provider.importer.FhirTerminologyImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.security.User
 import uk.ac.ox.softeng.maurodatamapper.terminology.CodeSet
@@ -43,24 +37,19 @@ import uk.ac.ox.softeng.maurodatamapper.terminology.CodeSetService
 import uk.ac.ox.softeng.maurodatamapper.terminology.Terminology
 import uk.ac.ox.softeng.maurodatamapper.terminology.TerminologyService
 import uk.ac.ox.softeng.maurodatamapper.traits.domain.MdmDomain
-import uk.ac.ox.softeng.maurodatamapper.version.Version
-import uk.ac.ox.softeng.maurodatamapper.version.VersionChangeType
-
-import org.springframework.beans.factory.annotation.Autowired
 
 class FhirFileModelImporterProviderService extends ImporterProviderService<MdmDomain, FhirFileModelImporterProviderServiceParameters>
     implements MetadataHandling, JsonImportMapping {
 
     private static List<String> BUNDLE_NON_METADATA_KEYS = ['id', 'entry']
 
-    AuthorityService authorityService
     TerminologyService terminologyService
     FhirTerminologyImporterProviderService fhirTerminologyImporterProviderService
     FhirCodeSetImporterProviderService fhirCodeSetImporterProviderService
     VersionedFolderService versionedFolderService
-    FolderService folderService
     CodeSetService codeSetService
     ClassifierService classifierService
+    AuthorityService authorityService
 
     @Override
     String getDisplayName() {
@@ -117,33 +106,33 @@ class FhirFileModelImporterProviderService extends ImporterProviderService<MdmDo
             // Import FHIR ValueSet as Mauro CodeSet
             domain = importCodeSet(user, data, params)
         } else if (data.resourceType == 'Bundle' && data.type == 'collection') {
-            domain = new VersionedFolder(label: data.id, createdBy: user.emailAddress, authority: authorityService.getDefaultAuthority())
-//            domain.parentFolder = folderService.findDomainByLabel('Federation tests')
+            // Import FHIR Bundle as Mauro VersionedFolder
+            domain = new VersionedFolder(label: data.id, createdBy: user.emailAddress, authority: params.providerSetAuthority ?: authorityService.getDefaultAuthority())
             processMetadata(data, domain, namespace, BUNDLE_NON_METADATA_KEYS)
             versionedFolderService.checkFacetsAfterImportingMultiFacetAware(domain)
             versionedFolderService.validate(domain)
             versionedFolderService.save(domain)
-            domain = versionedFolderService.get(domain.id)
 
             data.entry.findAll {it.resource?.resourceType == 'CodeSystem'}.each {Map<String, Object> codeSystemEntry ->
                 println 'CodeSystemEntry = ' + codeSystemEntry.toMapString()
                 Terminology terminology = importTerminology(user, codeSystemEntry.resource, params)
                 terminology.createdBy = user.emailAddress
-                terminology.authority = authorityService.getDefaultAuthority()
+                terminology.authority = params.providerSetAuthority ?: authorityService.getDefaultAuthority()
                 terminology.folder = domain
                 terminologyService.checkImportedTerminologyAssociations(user, terminology)
                 terminologyService.validate(terminology)
-                terminologyService.saveModelWithContent(terminology)
+                terminologyService.save(terminology)
             }
+
 
             data.entry.findAll {it.resource?.resourceType == 'ValueSet'}.each {Map<String, Object> valueSetEntry ->
                 println 'ValueSetEntry = ' + valueSetEntry.toMapString()
                 CodeSet codeSet = importCodeSet(user, valueSetEntry.resource, params)
                 codeSet.createdBy = user.emailAddress
-                codeSet.authority = authorityService.getDefaultAuthority()
+                codeSet.authority = params.providerSetAuthority ?: authorityService.getDefaultAuthority()
                 codeSet.folder = domain
                 codeSetService.validate(codeSet)
-                codeSetService.saveModelWithContent(codeSet)
+                codeSetService.save(codeSet)
             }
         } else {
             if (!user) throw new ApiUnauthorizedException('FHIRFILE02', 'Cannot import unsupported FHIR Resource Type')
@@ -187,12 +176,11 @@ class FhirFileModelImporterProviderService extends ImporterProviderService<MdmDo
             codeSetService.checkFinaliseModel(importedDomain, params.finalised, params.importAsNewBranchModelVersion)
         } else if (importedDomain instanceof VersionedFolder) {
             versionedFolderService.checkFacetsAfterImportingMultiFacetAware(importedDomain)
-//            if (versionedFolderService.findLatestFinalisedModelByLabel(importedDomain.label)) versionedFolderService.checkBranchModelVersion(importedDomain, params.importAsNewBranchModelVersion, params.newBranchName, currentUser)
+            versionedFolderService.checkAuthority(currentUser, importedDomain, params.useDefaultAuthority)
+            versionedFolderService.checkDocumentationVersion(importedDomain, params.importAsNewDocumentationVersion, currentUser, params.providerSetAuthority)
+            versionedFolderService.checkBranchModelVersion(importedDomain, params.importAsNewBranchModelVersion, params.newBranchName, currentUser,
+                                                           params.providerSetAuthority)
             versionedFolderService.checkFinaliseModel(importedDomain, params.finalised, params.importAsNewBranchModelVersion)
-            VersionedFolder previous = VersionedFolder.byLabelAndFinalisedAndLatestModelVersion(importedDomain.label).get()
-            if (previous) {
-                importedDomain.modelVersion = Version.nextMajorVersion(previous.modelVersion)
-            }
         }
         importedDomain
     }
